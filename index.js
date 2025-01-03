f5d1b0d511f5fe789ee3058459688df991470c17
Elina <elliekarout@gmail.com>
2025-01-03 11:33:16 +0200

    step 5

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3200
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
+
 app.get(`/test`, (req, res) => {
   res.json({status:200, message:`ok`})
 })
@@ -24,6 +31,24 @@ app.get('/search', (req, res) => {
     else {
         res.status(500).json({status: 500,error: true,message: "you have to provide a search"})
     }
+})
+
+/////////////////////////////////////////////////////////////////////
+
+app.get(`/movies/create`, (req, res) => {
+    res.json({status:200, message:`create`})
+})
+
+app.get(`/movies/read`, (req, res) => {
+    res.json({status:200, data:movies })
+})
+
+app.get(`/movies/update`, (req, res) => {
+    res.json({status:200, message:`update`})
+})
+
+app.get(`/movies/delete`, (req, res) => {
+    res.json({status:200, message:`delete`})
 })
 
 app.listen(port, (error) => {
