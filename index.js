80f8942237051c2c2637346d5ebc6cfb916ab0cc
Elina <elliekarout@gmail.com>
2025-01-03 10:44:10 +0200

    fixed step3

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express()
 const port = 3200
 
 app.get('/test', (req, res) => {
-  res.send({status:200, message:"ok"})
+  res.json({status:200, message:"ok"})
 })
 
 app.get('/time', (req, res) => {
     const TIME = Date().split(' ')[4].split(':')[0]+':'+Date().split(' ')[4].split(':')[1]
-    res.send({status:200, message:TIME})
-  })
+    res.json({status:200, message:TIME})
+})
 
 app.listen(port, (error) => {
     if (!error){
